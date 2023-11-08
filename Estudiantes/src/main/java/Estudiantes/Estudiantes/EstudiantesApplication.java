2b4e079435ac6c574afd905c513e59af06b3431f
CmoraGalaz <103866034+CmoraGalaz@users.noreply.github.com>
2023-11-07 23:17:41 -0300

    Eureka

--- Estudiantes/src/main/java/Estudiantes/Estudiantes/EstudiantesApplication.java
+++ Estudiantes/src/main/java/Estudiantes/Estudiantes/EstudiantesApplication.java
@@ -2,10 +2,11 @@ package Estudiantes.Estudiantes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableEurekaClient
 public class EstudiantesApplication {
 
 	public static void main(String[] args) {
